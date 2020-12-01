1f277a6054dfb433470967786d438c3813f2e724
Ashaba John <jnashaba4@gmail.com>
2020-12-01 21:57:00 +0300

    Crash course on nodejs

---
+++ booksJson.js
@@ -0,0 +1,32 @@
+db.books.insert([
+    {
+        title: 'War and Peace',
+        genre: 'Historical Fiction',
+        author: 'Ash',
+        read: false
+    },
+    {
+        title: 'Beauty and the Beast',
+        genre: 'Historical Fiction',
+        author: 'MM',
+        read: true
+    },
+    {
+        title: 'Solving Puzzle Problems',
+        genre: 'Historical Fiction',
+        author: 'KK',
+        read: false
+    },
+    {
+        title: 'Imitation of Christ',
+        genre: 'Historical Truth',
+        author: 'Jesus',
+        read: true
+    },
+    {
+        title: 'Andela and the False',
+        genre: 'Present Comedy',
+        author: 'Jeremy',
+        read: false
+    }
+])
